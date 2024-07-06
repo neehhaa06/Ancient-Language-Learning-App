49e6a9c5143d48af0adc59e8b44574f1458560f6
Neha Prasad <164743370+neehhaa06@users.noreply.github.com>
2024-07-06 14:49:25 +0530

    Create userRoutes.js

---
+++ routes/userRoutes.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const router = express.Router();
+
+router.post('/signup', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ email, password: hashedPassword });
+    await user.save();
+    res.status(201).send('User created');
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).send('User not found');
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).send('Invalid credentials');
+    }
+    const token = jwt.sign({ userId: user._id }, 'secretkey');
+    res.status(200).send({ token });
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
+module.exports = router;
